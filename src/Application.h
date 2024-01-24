967a2eb83350a6e62cad37ac23d3c75bfe27751d
shahar <shaharrazlan@gmail.com>
2024-01-24 14:54:48 +0200

    Uplaoding Aplication class

---
+++ src/Application.h
@@ -0,0 +1,12 @@
+#include "BloomFilter.h"
+#include "InputOutput.h" // Assuming you have a header file for InputOutput
+#include "UrlManager.h"
+
+class Application {
+    private:
+    BloomFilter bloomFilter;
+public:
+    Application(){};
+    void run();
+
+};
