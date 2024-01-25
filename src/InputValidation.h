bde29ee23e65ac033887f09257a44999a5061003
Shahar Razlan <117972259+shaharrazlan@users.noreply.github.com>
2024-01-25 20:50:20 +0200

    Update InputValidation.h

--- src/InputValidation.h
+++ src/InputValidation.h
@@ -2,34 +2,19 @@
 #define INPUT_VALIDATION_H
 
 #include <vector>
-<<<<<<< Updated upstream
-#include <cctype>
-#include <string>
-#include <iostream>
-#include <regex>
-=======
 #include <string>
 #include <iostream>
 
->>>>>>> Stashed changes
 
 class InputValidation {
 
 public:
-<<<<<<< Updated upstream
-    InputValidation();
-=======
   
->>>>>>> Stashed changes
     bool isANumber(std::vector<std::string> inputVector);
     bool isValidInputNumbers(std::vector<std::string> inputVector);
     std::vector<int> convertToInts(std::vector<std::string> inputVector);
     bool isValidNumberAndUrl(std::vector<std::string> inputVector);
-<<<<<<< Updated upstream
-    /*bool isValidUrl(std::string input);*/
-=======
  
->>>>>>> Stashed changes
 
 };
 
