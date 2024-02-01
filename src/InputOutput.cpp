75c7f94c1b51fd8c1ceed41667d9528b32493bc1
Shahar Razlan <117972259+shaharrazlan@users.noreply.github.com>
2024-02-01 13:25:19 +0200

    Update InputOutput.cpp

--- src/InputOutput.cpp
+++ src/InputOutput.cpp
@@ -1,6 +1,6 @@
 #include "InputOutput.h"
 
-
+// Method to extract string elements from an input line
 std::vector<std::string> InputOutput::getStringElements(std::string inputLine) {
     std::vector<std::string> result;
     std::istringstream iss(inputLine);
@@ -15,31 +15,29 @@ std::vector<std::string> InputOutput::getStringElements(std::string inputLine) {
     return result;
 }
 
- std::vector<std::string>  InputOutput::getData() {
+// Method to get the stored data
+std::vector<std::string> InputOutput::getData() {
     return data;
- }
+}
 
-
+// Method to get the action input from the user based on a boolean parameter
 std::string InputOutput::getAction(bool b) {
     std::string userInput;
     std::cin.clear();
     std::getline(std::cin, userInput);
     std::vector<std::string> inputVec = getStringElements(userInput);
     this->data = inputVec;
-   
-    if(b) {
-        if (iv->isValidNumberAndUrl(inputVec)) {
-                return inputVec[0];
-            } else {
-                return "0";
-            }
-        }
-    else if(iv->isValidInputNumbers(inputVec)) {
-        return "3";
-    } 
-    else {
-                return "0";
-            }
 
+    if (b) {
+        // Check if the input is a valid number and URL combination
+        if (iv->isValidNumberAndUrl(inputVec)) {
+            return inputVec[0];
+        } else {
+            return "0";  // Indicate an invalid input
+        }
+    } else if (iv->isValidInputNumbers(inputVec)) {
+        return "3";  // Indicate a valid numeric input
+    } else {
+        return "0";  // Indicate an invalid input
+    }
 }
- 
