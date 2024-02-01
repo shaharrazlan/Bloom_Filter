1b79e5ad13eccdb81a6a627b42dcb268a0c1b137
Shahar Razlan <117972259+shaharrazlan@users.noreply.github.com>
2024-02-01 13:18:10 +0200

    Update Application.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,14 +1,16 @@
 #include "Application.h"
 
+// Implementation of the run method in the Application class
 void Application::run(std::map<std::string, ICommand*> commands, BloomFilter* bf) {
     while (true) {
-        InputOutput* io = new InputOutput();
+        InputOutput* io = new InputOutput();  // Create a new instance of InputOutput
         std::string action;
-        action = io->getAction(bf->areParametersSet());
+        action = io->getAction(bf->areParametersSet());  // Get the action input from the user
 
         // Check if the command key exists in the map
         if (commands.find(action) != commands.end()) {
             try {
+                // Execute the command associated with the provided action
                 commands[action]->execute(io->getData(), bf);
             } catch (...) {
                 // Handle any exceptions thrown during command execution
@@ -20,7 +22,5 @@ void Application::run(std::map<std::string, ICommand*> commands, BloomFilter* bf
         }
     }
 }
-
-    
 
 // Other member functions of the Application class go here
