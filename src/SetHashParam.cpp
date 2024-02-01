240f06e0a8a8a84dd3b96c3fdb9f6f5030983508
SarahLampert <96434903+SarahLampert@users.noreply.github.com>
2024-02-01 13:28:35 +0200

    Update SetHashParam.cpp

--- src/SetHashParam.cpp
+++ src/SetHashParam.cpp
@@ -1,5 +1,8 @@
 #include "SetHashParam.h"
 
+// Implementation of the execute function from the SetHashParam class
 void SetHashParam::execute(std::vector<std::string> input, BloomFilter* bf) {
+
+    // Call the setParams function of the BloomFilter object with the provided input
     bf->setParams(input);
 }
