fb90d0022c76e67c1d3bbe56f971473567108cdf
SarahLampert <96434903+SarahLampert@users.noreply.github.com>
2024-02-01 13:27:36 +0200

    Update SetHashParam.h

--- src/SetHashParam.h
+++ src/SetHashParam.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "ICommand.h"
 
+// Class definition for SetHashParam, derived from ICommand
 class SetHashParam : public ICommand {
 public:
+
+// Constructor for SetHashParam
     SetHashParam(){};
+
+// Destructor for SetHashParam
     ~SetHashParam(){};
+
+// Implementation of the execute function from the ICommand interface
     void execute(std::vector<std::string> input, BloomFilter* bf);
 };
