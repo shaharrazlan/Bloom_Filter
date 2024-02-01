cdd1427ad7c349b8a1928ec0ada06cc318cf26bf
Shahar Razlan <117972259+shaharrazlan@users.noreply.github.com>
2024-02-01 13:18:40 +0200

    Update CheckUrl.h

--- src/CheckUrl.h
+++ src/CheckUrl.h
@@ -2,14 +2,19 @@
 
 #include "ICommand.h"
 
-
+// Class definition for the CheckUrl command, which is derived from ICommand
 class CheckUrl : public ICommand {
 private:
-    UrlManager* manager;
+    UrlManager* manager;  // Pointer to an instance of UrlManager for managing URLs
 
 public:
+    // Constructor for CheckUrl
     CheckUrl();
+
+    // Destructor for CheckUrl
     ~CheckUrl();
 
+    // Implementation of the execute method from ICommand
+    // Takes a vector of strings as input representing URLs and a BloomFilter for URL filtering
     void execute(std::vector<std::string> input, BloomFilter* bf);
 };
