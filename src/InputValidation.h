5d22424a79088136975a3e91dff90d62853fb2cd
SarahLampert <96434903+SarahLampert@users.noreply.github.com>
2024-02-01 13:32:13 +0200

    Update InputValidation.h

--- src/InputValidation.h
+++ src/InputValidation.h
@@ -7,16 +7,25 @@
 #include <iostream>
 #include <regex>
 
+// Class definition for InputValidation
 class InputValidation {
 
 public:
-  
+
+    // Function to check if all elements in the vector are numbers
     bool isANumber(std::vector<std::string> inputVector);
+
+    // Function to check if all elements in the vector are valid input numbers
     bool isValidInputNumbers(std::vector<std::string> inputVector);
+
+    // Function to convert vector elements to integers
     bool convertToInts(std::vector<std::string> inputVector);
+
+    // Function to check if input consists of a valid combination of numbers and URLs
     bool isValidNumberAndUrl(std::vector<std::string> inputVector);
  
 
 };
 
+// End of header guard
 #endif // INPUT_VALIDATION_H
