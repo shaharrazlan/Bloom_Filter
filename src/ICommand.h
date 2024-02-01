e7a3026678817c326e3497123e195e4c55f770cb
Shahar Razlan <117972259+shaharrazlan@users.noreply.github.com>
2024-02-01 13:20:06 +0200

    Update ICommand.h

--- src/ICommand.h
+++ src/ICommand.h
@@ -4,8 +4,11 @@
 #include <string>
 #include <vector>
 
+// Abstract base class definition for ICommand
 class ICommand {
-
-     public: virtual void execute(std::vector<std::string> input, BloomFilter* bf) = 0; 
-
+public:
+    // Pure virtual function representing the execute method
+    // Takes a vector of strings as input representing commands and a BloomFilter for URL filtering
+    virtual void execute(std::vector<std::string> input, BloomFilter* bf) = 0;
+    // "= 0" indicates that this method is a pure virtual function and must be overridden by derived classes
 };
