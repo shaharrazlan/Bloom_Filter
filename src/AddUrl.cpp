2ce67a00dcd5fcf47cc626bd2a0e84267fa331e6
Shahar Razlan <117972259+shaharrazlan@users.noreply.github.com>
2024-02-01 13:16:02 +0200

    Update AddUrl.cpp

--- src/AddUrl.cpp
+++ src/AddUrl.cpp
@@ -1,15 +1,17 @@
 #include "AddUrl.h"
 
+// Constructor implementation
 AddUrl::AddUrl() : manager(new UrlManager()) {
-    // Constructor implementation
+    // Create a new instance of UrlManager and assign it to the 'manager' member
 }
 
+// Destructor implementation
 AddUrl::~AddUrl() {
-    delete manager;
-    // Destructor implementation
+    delete manager;  // Deallocate memory for the UrlManager instance
 }
 
+// Implementation of the execute method
 void AddUrl::execute(std::vector<std::string> input, BloomFilter* bf) {
-    bf->addElement(input[1]);
-    manager->addUrl(input[1]);
+    bf->addElement(input[1]);  // Add the specified URL to the BloomFilter
+    manager->addUrl(input[1]);  // Add the specified URL to the UrlManager
 }
