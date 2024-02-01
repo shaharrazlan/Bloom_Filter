a6f47f26637677d2ded8ee1eb48e2380a3fcf98e
Shahar Razlan <117972259+shaharrazlan@users.noreply.github.com>
2024-02-01 13:16:57 +0200

    Update Application.h

--- src/Application.h
+++ src/Application.h
@@ -5,9 +5,13 @@
 #include "ICommand.h"
 #include <map>
 
+// Class definition for the Application class
 class Application {
 
 public:
+    // Default constructor for the Application class
     Application(){};
+
+    // Method to run the application, taking a map of commands and a BloomFilter as parameters
     void run(std::map<std::string, ICommand*> commands, BloomFilter* bf);
 };
