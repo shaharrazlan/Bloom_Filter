f66388c96bc97750116657131c4346b09313f0b7
Shahar Razlan <117972259+shaharrazlan@users.noreply.github.com>
2024-02-01 13:15:16 +0200

    Update AddUrl.h

--- src/AddUrl.h
+++ src/AddUrl.h
@@ -2,14 +2,19 @@
 #include "ICommand.h"
 #include "UrlManager.h"
 
-
+// Class definition for the AddUrl command, which is derived from ICommand
 class AddUrl : public ICommand {
 private:
-    UrlManager* manager;
+    UrlManager* manager;  // Pointer to an instance of UrlManager for managing URLs
 
 public:
-     AddUrl();
+    // Constructor for AddUrl
+    AddUrl();
+
+    // Destructor for AddUrl
     ~AddUrl();
 
+    // Implementation of the execute method from ICommand
+    // Takes a vector of strings as input representing URLs and a BloomFilter for URL filtering
     void execute(std::vector<std::string> input, BloomFilter* bf);
 };
